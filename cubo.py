6c08ecb04a9b33498f0db4f348db96a027eaa5be
Angelo Caravella <sannita6570@gmailcom>
2018-03-02 19:17:11 +0100

    il cubo di un numero

---
+++ cubo.py
@@ -0,0 +1,6 @@
+#cubo.py
+#Crea un programma cubo.py che stampa il volume di un cubo dato il lato (intero)
+print " calcola il cubo del lato"
+lato= input ("Numero:")
+cubo = lato *lato * lato
+print " Il cubo di" , lato , "e'" , cubo
