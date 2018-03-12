9473a68feedb20dbd1dec386a95ab934bddabd64
Angelo Caravella <sannita6570@gmailcom>
2018-03-12 11:35:56 +0100

    equazione di secondo grado

--- PrimoGrado.py
+++ PrimoGrado.py
@@ -9,13 +9,11 @@ print "nella forma Ax + B = 0\n"
 A = input('A: ')
 B = input('B: ')
 
-if A != 0 and B != 0 :
-    x = -float(B) / float(A)
-    print "L'equazione ammette la  soluzione:",x 
-
-elif A == 0 and B == 0 :
+if A==0 and B==0:
     print "L'equazione 0 = 0 ammette infinite soluzioni"
 
-elif A == 0 :
-    print "L'equazione", B, "= 0 non ammette soluzioni"
-
+elif A==0:
+        print "L'equazione", B, "= 0 non ammette soluzioni"
+else:
+        x = -float(B) / float(A)
+        print "L'equazione ammette soluzione:", x
